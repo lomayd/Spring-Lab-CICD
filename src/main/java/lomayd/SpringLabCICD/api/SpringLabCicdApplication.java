8d1e2751aadcfe1cff24ce2e0dc3cac6775586f8
lomayd <jinho10167@gmail.com>
2024-03-13 12:53:09 +0900

    feat: Project Initialize

---
+++ src/main/java/lomayd/SpringLabCICD/api/SpringLabCicdApplication.java
@@ -0,0 +1,13 @@
+package lomayd.SpringLabCICD.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLabCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lomayd/SpringLabCICD/SpringLabCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package lomayd.SpringLabCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
